2dff295662e3dc961adb9582979d20d755d8bf32
Andrew Barlow <dandrewbarlow@gmail.com>
2020-01-25 20:28:19 -0700

    beginning js

---
+++ js/shifty.js
@@ -0,0 +1,12 @@
+var elements;
+shiftVal = document.getElementsByClassName("body").style.left;
+
+while (1) {
+  var left = 0;
+  var delta = 1;
+  elements = left;
+  if (left >= 100 | left <= 0) {
+    delta = -delta;
+  }
+  left += delta;
+}
